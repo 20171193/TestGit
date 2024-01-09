e501233c4a3f6f04548198a5b092b00339c8b27d
Shim Jae Chun <shimnaldo98@gmail.com>
2024-01-09 11:05:30 +0900

    stash 테스트

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,8 +1,9 @@
 ﻿// git
 // stage = 변경사항 올리기
 // commit = 변경사항을 버전에 기록하기
+// diff = 변경과 현재사항 확인
+// stash = 기록되지 않은 내용을 임시저장소에 잠시 보관
 // 
-//
 
 namespace TestGit
 {
@@ -16,5 +17,14 @@ namespace TestGit
     {
         public int hp;
         public void TakeDamage(Player owner) { }
+    }
+    public class Skill
+    {
+        // stash 후 작업
+    }
+
+    public class Item
+    {
+        // 작업도중 stash
     }
 }
