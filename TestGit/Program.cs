c996392415b89bc0ee3110250aaf018cdcb418ed
Shim Jae Chun <shimnaldo98@gmail.com>
2024-01-09 10:44:00 +0900

    Monster 클래스 추가

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,8 +1,20 @@
-﻿namespace TestGit
+﻿// git
+// stage = 변경사항 올리기
+// commit = 변경사항을 버전에 기록하기
+// 
+//
+
+namespace TestGit
 {
     public class Player
     {
         public int hp;
         public void Attack() { }
+    }
+
+    public class Monster
+    {
+        public int hp;
+        public void TakeDamage(Player owner) { }
     }
 }
